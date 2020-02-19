9ab3d1d5e39f8359d55152b684ab27af21f7a7c2
T3chy <elam.day.friedland@gmail.com>
2020-02-19 10:40:19 -0500

    did the spreadsheet downlaoder thing kinda

--- Scriptz/get_docs_and_scores_from_xlsx.py
+++ Scriptz/get_docs_and_scores_from_xlsx.py
@@ -1,6 +1,15 @@
+import qq
 import xlrd
-loc = '/home/t3chy/venvs/Rusich_Grader/Sample.xlsx' 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-print(sheet.cell_value(1, 1)) 
-
+import glob, os
+def downloadfromsheet(loc):
+	wb = xlrd.open_workbook(loc) 
+	sheet = wb.sheet_by_index(0) 
+	print(sheet.cell_value(1, 1)) 
+	for i in range(sheet.nrows):
+		fileid = sheet.cell_value(i,5)
+		qq.main(fileid)
+		print('download completed for file'+i+'of'+sheet.nrows)
+sss = []
+os.chdir('/home/t3chy/venvs/Rusich_Grader/')
+for file in glob.glob("*.xlsx"):
+    sss.append(file)
