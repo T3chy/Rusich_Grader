4408fdd0bb348227a331888f8a43321483a5594a
T3chy <elam.day.friedland@gmail.com>
2020-02-19 11:34:38 -0500

    aaa

--- Scriptz/get_docs_and_scores_from_xlsx.py
+++ Scriptz/get_docs_and_scores_from_xlsx.py
@@ -4,12 +4,19 @@ import glob, os
 def downloadfromsheet(loc):
 	wb = xlrd.open_workbook(loc) 
 	sheet = wb.sheet_by_index(0) 
-	print(sheet.cell_value(1, 1)) 
 	for i in range(sheet.nrows):
-		fileid = sheet.cell_value(i,5)
+		if i == 0:
+			continue
+		print(i)
+		fileid = sheet.cell_value(i,4)
+		print(fileid)
 		qq.main(fileid)
 		print('download completed for file'+i+'of'+sheet.nrows)
 sss = []
 os.chdir('/home/t3chy/venvs/Rusich_Grader/')
 for file in glob.glob("*.xlsx"):
     sss.append(file)
+print(sss)
+for ss in sss:
+	print(ss)
+	downloadfromsheet(ss)
