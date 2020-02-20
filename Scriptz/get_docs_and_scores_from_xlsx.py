7f4e92d37b2edaaca2ac6a0bb9d967af05b9b0e9
T3chy <elam.day.friedland@gmail.com>
2020-02-19 20:46:09 -0500

    yes

--- Scriptz/get_docs_and_scores_from_xlsx.py
+++ Scriptz/get_docs_and_scores_from_xlsx.py
@@ -11,7 +11,8 @@ def downloadfromsheet(loc):
 		fileid = sheet.cell_value(i,4)
 		print(fileid)
 		qq.main(fileid)
-		print('download completed for file'+i+'of'+sheet.nrows)
+		print('download completed for file '+str(i)+' of '+str(sheet.nrows))
+	print('Download Complete!')
 sss = []
 os.chdir('/home/t3chy/venvs/Rusich_Grader/')
 for file in glob.glob("*.xlsx"):
--- Scriptz/print10ids.py
+++ Scriptz/print10ids.py
@@ -4,15 +4,14 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import io
-import os
-import shutil
-from io import BytesIO
-from googleapiclient.http import MediaIoBaseDownload
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.file']
 
-def main(id):
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -33,16 +32,18 @@ def main(id):
             pickle.dump(creds, token)
 
     service = build('drive', 'v3', credentials=creds)
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-        service = build('drive', 'v3', credentials=creds)
-        file_id = str(id)
-        request = service.files().export_media(fileId=file_id,
-         mimeType='plain/text')
-        fh = io.FileIO(id + '.txt', 'wb')
-        downloader = MediaIoBaseDownload(fh, request)
-  #      done = False
- #       while done is False:
- #           status, done = downloader.next_chunk()
-#            print("Download %d%%." % int(status.progress() * 100))
 
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+if __name__ == '__main__':
+    main()
